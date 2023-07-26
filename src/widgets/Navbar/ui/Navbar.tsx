3efbb7deebc424f9b1ed834375745b24dcca4efb
Evgeniy Shlenskiy <shlenskiyevgeniy@gmail.com>
2023-07-26 10:12:30 +0300

    1

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,13 +1,10 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink";
 import cls from "./Navbar.module.scss";
-import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
     className?: string;
 }
 export function Navbar({ className }: NavbarProps) {
-    const { t } = useTranslation();
     return (
         <nav className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.Navbar__container}></div>
